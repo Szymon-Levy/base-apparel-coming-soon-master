d235f6ea6226df6c6a0e7d3cc1f661554a4fb28a
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2022-11-06 16:47:43 +0100

    done

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const EmailForm = document.getElementById("Email-form");
+const emailForm = document.getElementById("Email-form");
+const emailRegEx = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
-EmailForm.addEventListener("submit", () =>{
-    let input = EmailForm.querySelector("input");
-    if (input.value == false){
-        EmailForm.classList.add("invalid");
 
+emailForm.addEventListener("submit", () =>{
+    let input = emailForm.querySelector("input");
+    if (!emailRegEx.test(input.value)){
+        emailForm.classList.add("invalid");
     }
     else{
-        EmailForm.classList.remove("invalid");
+        emailForm.classList.remove("invalid");
     }
 })
